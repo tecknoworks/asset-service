0f7b3446ee09e6a8f4d7fb250a6cf9237938dd0b
Plotkins <geo.sava1@gmail.com>
2019-08-23 18:08:01 +0300

    Refactored the structure.

--- src/index.js
+++ src/index.js
@@ -7,16 +7,12 @@ const serveStatic = require('serve-static')
 const fileUpload = require('express-fileupload');
 
 const port = 3002
-const router= require('./src/api/index')
+const router= require('./api/index')
 
 app.use(cors())
 app.use(bodyparser.json())
 app.use(fileUpload())
 
-console.log(path.join(__dirname, 'assets'));
-
 app.use('/assets', router)
-// app.use(serveStatic(path.join(__dirname, 'assets')))
 
 app.listen(port, ()=> console.log( `Assets microservice listening on port ${port}`));
-
