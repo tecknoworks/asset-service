d1c7ea8080b76b5f551a9efec3ff51c65de5b28f
Plotkins <geo.sava1@gmail.com>
2019-08-27 19:54:52 +0300

    Installed nodemon and morgan.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ const express = require('express')
 const bodyparser= require('body-parser')
 const cors = require('cors')
 const path = require('path')
-const app = express()
 const serveStatic = require('serve-static')
 const fileUpload = require('express-fileupload');
+const logger = require('morgan');
+
+const app = express()
 
 const port = 3002
 const router= require('./api/index')
 
 app.use(cors())
 app.use(bodyparser.json())
+app.use(logger('dev'));
 app.use(fileUpload())
 
 app.use('/assets', router)
